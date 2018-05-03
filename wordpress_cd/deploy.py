c3761b79504e26ab038d340aace813ea94db8223
Ross Golder <ross@golder.org>
2018-05-03 18:23:57 +0700

    Add 'unpack_artefact' method.

--- wordpress_cd/deploy.py
+++ wordpress_cd/deploy.py
@@ -1,9 +1,11 @@
-import os
-
+import sys, os
+import tempfile
+import subprocess
 import logging
 _logger = logging.getLogger(__name__)
 
 import wordpress_cd.drivers as drivers
+from wordpress_cd.build import get_artefact_dir
 
 
 def _driver(args):
@@ -47,3 +49,28 @@ def deploy_site(args):
     driver = _driver(args)
     _logger.info("Deploying site using {0} driver...".format(driver))
     return driver.deploy_site()
+
+def unpack_artefact():
+    # Determine artefact filename and presence
+    work_dir = os.getcwd()
+    module_id = os.getenv("JOB_BASE_NAME", os.path.basename(os.getcwd()))
+    zip_file = "{0}/{1}.zip".format(get_artefact_dir(work_dir), module_id)
+
+    # Make temporary directory and move to it
+    tmp_dir = tempfile.mkdtemp()
+    os.chdir(tmp_dir)
+
+    # Unpack the artefact
+    _logger.info("Unpacking module build artefact '{0}'...".format(module_id))
+
+    # Sync new site into place, leaving config/content in place
+    unpackargs = [
+        "unzip", zip_file
+    ]
+    exitcode = subprocess.call(unpackargs)
+    _logger.debug("unzip exitcode: {0}".format(exitcode))
+    if exitcode != 0:
+        logger.error("Unable to unpack build artefact. Exit code: {0}".format(exitcode))
+        return exitcode
+
+    return tmp_dir
