9b53888fda8326e72170bc0931d515fa617f0c84
Ross Golder <ross@golder.org>
2019-06-29 11:47:28 +0700

    Bump version to 0.7.4.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'wordpress-cd',
-    version = '0.7.3',
+    version = '0.7.4',
     description = 'Helper scripts to assist with WordPress-related CI/CD and devops tasks (i.e. build/test/deploy workflows for use with GitLab/Jenkins etc)',
     author = 'Ross Golder',
     author_email = 'ross@golder.org',
